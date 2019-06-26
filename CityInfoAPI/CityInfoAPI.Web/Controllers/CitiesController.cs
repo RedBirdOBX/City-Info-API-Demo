86395b112fc50856dfaf73e7bbdc4f3879c3630e
Taradel-DSF <shane@taradel.com>
2019-06-26 08:54:43 -0400

    adding xml comments and swashbuckle

--- CityInfoAPI/CityInfoAPI.Web/Controllers/CitiesController.cs
+++ CityInfoAPI/CityInfoAPI.Web/Controllers/CitiesController.cs
@@ -22,8 +22,10 @@ namespace CityInfoAPI.Web.Controllers
             _logger = logger;
         }
 
-        // Get all cities
-        // http://{domain}/api/cities
+        //  Get all cities
+        //  http://{domain}/api/cities
+        /// <summary>get a collection of all cities.</summary>
+        /// <returns>a collection of all cities</returns>
         [HttpGet("", Name = "GetCities")]
         public IActionResult GetCities()
         {
@@ -33,6 +35,10 @@ namespace CityInfoAPI.Web.Controllers
 
         // Get city by id
         // http://{domain}/api/cities/{cityId}
+        /// <summary>get a specific city by id</summary>
+        /// <param name="cityId">the id of the city</param>
+        /// <param name="includePointsOfInterest">flag which indicates whether or not points of interest should be included.  true/false</param>
+        /// <returns>a city with optional points of interest</returns>
         [HttpGet("{cityId}", Name = "GetCityById")]
         public IActionResult GetCityById([FromRoute] int cityId, [FromQuery] bool includePointsOfInterest = false)
         {
