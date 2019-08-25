77b1b92518ed4f2cf5643692e3c72fd3a91826dc
RedBirdOBX <shane.fowlkes.70@gmail.com>
2019-08-25 17:56:32 -0400

    wip

---
+++ CityInfoAPI/CityInfoAPI.Logic/Authentication/BasicAuthenticationHandler.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace CityInfoAPI.Logic.Authentication
+{
+    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public BasicAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock) : base(options, logger, encoder, clock)
+        {
+        }
+
+        protected override Task<AuthenticationResult> HandleAuthenticationAsync()
+        {
+        }
+    }
+}
